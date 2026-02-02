0e246add8eed87ea827ec321af5c56fde16e6950
lmpaCsharp <lmpa@cin.ufpe.br>
2026-02-02 20:24:21 -0300

    feat(Mazes/Scenes/License) Added license txt, mostly fixed spike timing, added extra controls and fixed maze4 crash bug.

--- Scenes/SceneObjects/mazes/tilemap_maze_5.gd
+++ Scenes/SceneObjects/mazes/tilemap_maze_5.gd
@@ -1,12 +1,24 @@
 extends Node2D
 
-
 @export var spike_timer := 0.0
 
 
-func _process(delta: float) -> void:
-	if spike_timer >= 2.5:
+
+func _ready():
+		get_node("Spikes").collision_enabled = true
+	
+
+
+func _physics_process(delta: float) -> void:
+	
+	
+	#for i in spike_timer:
+	#	print(spike_timer)
+	
+	if spike_timer >= 2.0:
 		$Spikes.collision_enabled = !$Spikes.collision_enabled
 		#print($Spikes.collision_enabled)
 		spike_timer = 0.0
+		get_node("Spikes").update_internals()
 	spike_timer += delta
+	
